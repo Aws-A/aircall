336d3237290edaaf1031406465877931a05e4c36
Aws-A <awsalrahim@yahoo.com>
2025-01-23 11:51:43 -0500

    Archive Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,54 @@ import Header from './Header.jsx';
 const App = () => {
   const [showArchived, setShowArchived] = useState(true); // State to toggle between archived and unarchived calls
   const [selectedCall, setSelectedCall] = useState(null); // Track the currently selected call
+  const [archivedCalls, setArchivedCalls] = useState([
+    { id: 'call1', name: 'Ryan L.', img: './images/call.png', date: 'Wednesday 22/01/2025', time: '20:20 P.M', duration: '15:49' },
+    { id: 'call2', name: 'Diana Williams', img: './images/missed.png', date: 'Tuesday 21/01/2025', time: '21:00 P.M', duration: '20:10' },
+    { id: 'call3', name: 'Nadia D.', img: './images/received.png', date: 'Monday 20/01/2025', time: '09:15 A.M', duration: '5:40' },
+  ]); // Archived calls
+  const [unarchivedCalls, setUnarchivedCalls] = useState([
+    { id: 'call4', name: '647-555-7777', img: './images/received.png', date: 'Sunday 19/01/2025', time: '15:30 P.M', duration: '8:20' },
+    { id: 'call5', name: '416-931-9311', img: './images/noresponse.png', date: 'Saturday 18/01/2025', time: '11:00 A.M', duration: '7:40' },
+  ]); // Unarchived calls
 
   const handleToggle = () => {
-    setShowArchived(!showArchived); // Toggle state
+    setShowArchived(!showArchived); // Toggle between archived and unarchived views
   };
 
-  function showdetails1(event, callId) {
-    if (selectedCall === callId) {
-      setSelectedCall(null); // Hide the details if the same call is clicked again
-    } else {
-      setSelectedCall(callId); // Show the details of the clicked call
+  const showDetails = (callId) => {
+    // Only allow showing details for calls in the archived list
+    if (archivedCalls.some((call) => call.id === callId)) {
+      setSelectedCall((prevCall) => (prevCall === callId ? null : callId)); // Toggle details
     }
-  }
+  };
+
+  const archiveAll = () => {
+    // Move all unarchived calls to archived
+    setArchivedCalls([...archivedCalls, ...unarchivedCalls]);
+    setUnarchivedCalls([]); // Clear unarchived calls
+  };
+
+  const unarchiveAll = () => {
+    // Move all archived calls to unarchived
+    setUnarchivedCalls([...unarchivedCalls, ...archivedCalls]);
+    setArchivedCalls([]); // Clear archived calls
+  };
+
+  const renderCall = (call) => (
+    <React.Fragment key={call.id}>
+      <div className="calls" onClick={() => showDetails(call.id)}>
+        <span id={call.id}>{call.name}</span>
+        <img src={call.img} className="small-img" />
+      </div>
+      {selectedCall === call.id && archivedCalls.some((c) => c.id === call.id) && (
+        <div className={`details ${call.id}`}>
+          <span className="date">{call.date}</span>
+          <span className="time">{call.time}</span>
+          <div className="duration">Time: {call.duration}</div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 
   return (
     <div className="container">
@@ -40,78 +76,23 @@ const App = () => {
         </label>
       </div>
       <div id="allCalls">
-        {showArchived ? (
-          <div id="archived">
-            <div className="calls" onClick={(e) => showdetails1(e, 'call1')}>
-              <span id="call1"> Ryan L. </span>
-              <img src="./images/call.png" className="small-img" />
-            </div>
-            {selectedCall === 'call1' && (
-              <div className="details details1">
-                <span className='date'> Wednesday 22/01/2025</span>
-                <span className='time'> 20:20 P.M </span>
-                <div className='duration'> Time: 15: 49 </div>
-              </div>
-            )}
-            <div className="calls" onClick={(e) => showdetails1(e, 'call2')}>
-              <span id="call2"> Diana Williams </span>
-              <img src="./images/missed.png" className="small-img" />
-            </div>
-            {selectedCall === 'call2' && (
-              <div className="details details2">
-                <span className='date'> Tuesday 21/01/2025</span>
-                <span className='time'> 21:00 P.M </span>
-                <div className='duration'> Time: 20: 10 </div>
-              </div>
-            )}
-            <div className="calls" onClick={(e) => showdetails1(e, 'call3')}>
-              <span id="call3"> Nadia D. </span>
-              <img src="./images/received.png" className="small-img" />
-            </div>
-            {selectedCall === 'call3' && (
-              <div className="details details3">
-                <span className='date'> Monday    20/01/2025</span>
-                <span className='time'> 09:15 A.M </span>
-                <div className='duration'> Time: 5: 40 </div>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div id="unarchived">
-            <div className="calls" onClick={(e) => showdetails1(e, 'call4')}>
-              <span id="call4"> 647-555-7777 </span>
-              <img src="./images/received.png" className="small-img" />
-            </div>
-            {selectedCall === 'call4' && (
-              <div className="details details4">
-                <span className='date'> Sunday 19/01/2025</span>
-                <span className='time'> 15:30 P.M </span>
-                <div className='duration'> Time: 8: 20 </div>
-              </div>
-            )}
-            <div className="calls" onClick={(e) => showdetails1(e, 'call5')}>
-              <span id="call5"> 416-931-9311 </span>
-              <img src="./images/noresponse.png" className="small-img" />
-            </div>
-            {selectedCall === 'call5' && (
-              <div className="details details5">
-                <span className='date'> Saturday  18/01/2025</span>
-                <span className='time'> 11:00 A.M </span>
-                <div className='duration'> Time: 7:40 </div>
-              </div>
-            )}
-          </div>
-        )}
+        {showArchived
+          ? archivedCalls.map((call) => renderCall(call))
+          : unarchivedCalls.map((call) => renderCall(call))}
       </div>
-      <div id='buttons'>
-        <button className="btn archAll"> Archive All Calls</button>
-        <br/>
-        <button className="btn unarchAll"> Unarchive All Calls</button>
+      <div id="buttons">
+        <button className="btn archAll" onClick={archiveAll}>
+          Archive All Calls
+        </button>
+        <br />
+        <button className="btn unarchAll" onClick={unarchiveAll}>
+          Unarchive All Calls
+        </button>
       </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
